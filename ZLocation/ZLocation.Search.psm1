7a31bdbce6dc5a48b76d36e8ca781092d58ca2b1
Sergei Vorobev <sevoroby@microsoft.com>
2015-05-05 20:06:08 -0700

    Fix problem with relative paths. Issue #5

--- ZLocation/ZLocation.Search.psm1
+++ ZLocation/ZLocation.Search.psm1
@@ -18,6 +18,10 @@ function Find-Matches([hashtable]$hash, [string[]]$query)
 
 function Test-FuzzyMatch([string]$path, [string[]]$query)
 {
+    function contains([string]$left, [string]$right) {
+        return [bool]($left.IndexOf($right, [System.StringComparison]::OrdinalIgnoreCase) -ge 0)
+    }
+
     if ($query -eq $null) {
         return $true
     }
@@ -30,20 +34,35 @@ function Test-FuzzyMatch([string]$path, [string[]]$query)
 
     for ($i=0; $i -lt $n-1; $i++)
     {
-        if (-not ($path -match $query[$i]))
+        if (-not (contains -left $path -right $query[$i]))
         {
             return $false
         }
     }   
     
     # after tab expansion, we get desired full path as a last query element.
-    if ([System.IO.Path]::IsPathRooted($query[$n-1])) 
+    # tab expansion can come from our code, then it will represend the full path.
+    # It also can come from the standart tab expension (when our doesn't return anything), which is file system based. 
+    # It can produce relative paths.
+
+    $rootQuery = $query[$n-1]
+
+    if (-not [System.IO.Path]::IsPathRooted($rootQuery)) {
+        # handle '..\foo' case
+        $rootQueryCandidate = Join-Path $pwd $query[$n-1]
+        if (Test-Path $rootQueryCandidate) {
+            $rootQuery = (Resolve-Path $rootQueryCandidate).Path
+        }
+    }
+    
+    if ([System.IO.Path]::IsPathRooted($rootQuery)) 
     {
-        $rootQuery = $query[$n-1]
+        # doing a tweak to handle 'C:' and 'C:\' cases correctly.
         if (($rootQuery.Length) -eq 2 -and ($rootQuery[-1] -eq ':'))
         {
             $rootQuery = $rootQuery + "\"
         }
+        # doing a tweaks to handle 'C:\foo' and 'C:\foo\' cases correctly.
         elseif ($rootQuery[-1] -eq '\')
         {
             $rootQuery = $rootQuery.Substring(0, $rootQuery.Length-1)
@@ -52,7 +71,7 @@ function Test-FuzzyMatch([string]$path, [string[]]$query)
     }
 
     $leaf = Split-Path -Leaf $path
-    return ($leaf -match $query[$n-1]) 
+    return (contains -left $leaf -right $query[$n-1]) 
 }
 
 Export-ModuleMember -Function Find-Matches
