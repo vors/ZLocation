769c096c7c382f17814b35639bb62675bc7c1b06
Sergei Vorobev <sevoroby@microsoft.com>
2015-05-05 19:31:17 -0700

    Add test for leaf matches only

--- ZLocation.Tests/ZLocation.Search.Tests.ps1
+++ ZLocation.Tests/ZLocation.Search.Tests.ps1
@@ -1,42 +1,52 @@
 Import-Module $PSScriptRoot\..\ZLocation\ZLocation.Search.psm1 -Force
 
-Describe "Find-Matches filters results correctly" {
-    Context "Equal weight" {
-        $data = 
-        @{
+Describe 'Find-Matches filters results correctly' {
+    Context 'Equal weight' {
+        $data = @{
             'C:\foo1\foo2\foo3' = 1.0
             'C:\foo1\foo2' = 1.0
             'C:\foo1' = 1.0
             'C:\' = 1.0
         }
 
-        It "Does not modify data" {
+        It 'Does not modify data' {
             Find-Matches $data fuuuu
             $data.Count | Should be 4
         }
 
-        It "returns only leave result" {
+        It 'returns only leave result' {
             Find-Matches $data foo2 | Should Be 'C:\foo1\foo2'
         }
 
-        It "returns multiply results" {
+        It 'returns multiply results' {
             (Find-Matches $data foo | measure).Count | Should Be 3
         }
 
-        It "should be case-insensitive" {
+        It 'should be case-insensitive' {
             Find-Matches $data FoO1 | Should Be 'C:\foo1'
         }
 
-        It "returns disk root folder for C:" {
+        It 'returns disk root folder for C:' {
             Find-Matches $data C: | Should Be 'C:\'
         }
 
-        It "returns disk root folder for C" {
+        It 'returns disk root folder for C' {
             Find-Matches $data C | Should Be 'C:\'
         }
 
-        It "should ignore trailing \" {
+        It 'should ignore trailing \' {
             Find-Matches $data C:\foo1\ | Should Be 'C:\foo1'
+        }
+    }
+
+    Context 'Different weight' {
+        $data = @{
+            'C:\admin' = 1.0
+            'C:\admin\monad' = 2.0
+        }
+
+        It 'Use leaf matches' {
+            Find-Matches $data 'adm' | Should Be 'C:\admin'
         }
     }
 }
