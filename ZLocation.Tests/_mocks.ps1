a6f44db710dc031648260e51cf516903533bb1ec
Andrew Bradley <cspotcode@gmail.com>
2018-08-31 15:01:45 -0400

    Test mocking legacy backup location

--- ZLocation.Tests/_mocks.ps1
+++ ZLocation.Tests/_mocks.ps1
@@ -1,8 +1,12 @@
 # Use a testing database; do not touch user's primary database
 $testDb = Join-Path $PSScriptRoot '../ignored/z-location-tests.db'
+$legacyBackup = Join-Path $PSScriptRoot '../ignored/z-location-tests-legacy-backup.txt'
 if(-not (Test-Path (Split-Path -Parent $testdb))) { New-Item -Type Directory (Split-Path -Parent $testdb) }
 if(Get-Module -all ZLocation.Service) {
     Mock -ModuleName ZLocation.Service Get-ZLocationDatabaseFilePath {
+        $testDb
+    }.getNewClosure() -ErrorAction SilentlyContinue
+    Mock -ModuleName ZLocation.Service Get-ZLocationLegacyBackupFilePath {
         $testDb
     }.getNewClosure() -ErrorAction SilentlyContinue
 }
